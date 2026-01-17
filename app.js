b92d6ad270a121c18161b20f0cf62f5f2717d905
sarahroon <sarahrooney130500@gmail.com>
2026-01-17 13:57:23 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const image = document.querySelector("img");
-image.load();
-image.addEventListener("load", function (event) {
-  console.log("Image loaded!", event);
-});
-
 document.getElementById("playBtn").addEventListener("click", function ()
 document.getElementById("audio").play();
 });
