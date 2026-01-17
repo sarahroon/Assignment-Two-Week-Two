76e872fb04f2172655562f2d1fbe37437eede892
sarahroon <sarahrooney130500@gmail.com>
2026-01-17 16:40:14 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let button = document.getElementById("myButton");
 let audio= document.getElementById("myAudio");
 
-if (myButton && myAudio) {
-  button.addEventListener("click", () -> {
+if (button && audio) {
+  button.addEventListener("click", ()=> {
     if (audio.paused) {
       audio.play();
       button.textContent = "Pause Audio";
     } else {
       audio.pause();
-      button.textContent = "Play Audio"; {
-    
+      button.textContent = "Play Audio";
+    }
+  });
+}
