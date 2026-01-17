a46a080cc54cd4d69b2682a1d0405ae5c862ae3c
sarahroon <sarahrooney130500@gmail.com>
2026-01-17 13:57:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,27 +4,6 @@ image.addEventListener("load", function (event) {
   console.log("Image loaded!", event);
 });
 
-const audio = document.querySelector("audio");
-button.onclick = () => audio.play();
-const button = document.querySelector("button");
-button.addEventListener("click", function (event) {
-  audio.play();
-});
-
-const audio = document.querySelector("audio");
-button.onclick = () => audio.pause();
-const button = document.querySelector("button");
-button.addEventListener("click", function (event) {
-  audio.pause();
-});
-
-const audio = document.querySelector("audio");
-const slider = document.querySelector("input");
-slider.addEventListener("change", function (event) {
-  audio.volume = event.target.value;
-});
-
-
-
-
+document.getElementById("playBtn").addEventListener("click", function ()
+document.getElementById("audio").play();
 });
